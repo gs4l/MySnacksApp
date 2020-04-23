051f61b2b9d6782c78f8ec4aa3d8504728732b0e
gs4l <ankur.kumar17081998@gmail.com>
2020-04-23 21:46:47 +0530

    Initial Code

---
+++ app/src/main/java/com/snacks/snacks/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.snacks.snacks.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "http://0.0.0.0/";
+}
---
+++ app/src/main/java/com/snacks/snacks/utils/RetrofitInstance.java
@@ -0,0 +1,11 @@
+package com.snacks.snacks.utils;
+
+import retrofit2.Retrofit;
+public class RetrofitInstance {
+
+    private static final String BASE_URL = Constants.BASE_URL;
+
+    private static Retrofit retrofit;
+
+
+}
