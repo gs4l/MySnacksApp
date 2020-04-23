8b46df77fe5c88cdbc57c58a90e356df8fd95480
gs4l <ankur.kumar17081998@gmail.com>
2020-04-23 22:06:08 +0530

    created RetrofitInstance class

--- app/src/main/java/com/snacks/snacks/utils/RetrofitInstance.java
+++ app/src/main/java/com/snacks/snacks/utils/RetrofitInstance.java
@@ -7,5 +7,19 @@ public class RetrofitInstance {
 
     private static Retrofit retrofit;
 
+    private static Retrofit getRetrofitInstance(){
+
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .build();
+        }
+
+        return retrofit;
+    }
+
+    public static <S> S createService(Class<S> serviceClass){
+        return getRetrofitInstance().create(serviceClass);
+    }
 
 }
